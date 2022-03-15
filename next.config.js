1d2eb9f3dae24e78380d422e06c43fa82619b1a6
nino <d20200720@gmail.com>
2022-03-16 06:15:51 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const REPO_NAME = 'lofi';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  basePath: process.env.NODE_ENV === 'production' ? '/' + REPO_NAME : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' + REPO_NAME : '',
+  trailingSlash: true,
+};
